5089dccb1e9b9a02e2b23a8622be08e9cce78e50
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-05-24 00:39:05 -0400

    Delete unused pages

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -3,7 +3,6 @@
 import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { WagmiProvider } from 'wagmi'
 
@@ -13,16 +12,13 @@ import { wagmiConfig } from '@/lib/web3'
 const queryClient = new QueryClient()
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
-  const [isMounted, setIsMounted] = useState(false)
-  useEffect(() => setIsMounted(true), [])
-
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <StyledComponentsRegistry>
           <ThemeProvider theme={lightTheme}>
             <ThorinGlobalStyles />
-            <RainbowKitProvider>{isMounted && children}</RainbowKitProvider>
+            <RainbowKitProvider>{children}</RainbowKitProvider>
           </ThemeProvider>
         </StyledComponentsRegistry>
       </QueryClientProvider>
